608d33b7ff6588e44b215e53a867e7f570dedae7
legajo85042-4 <nexten.uixi@gmail.com>
2024-10-09 16:03:07 -0300

    Comienzo Migracion a SPA

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
- 
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
 
 
-console.log("funciona");
+async function getTrendingMoviesPreview(){
+    const {data} = await api('trending/movie/day');
+
+
+    const movies = data.results;
+    console.log({data, movies});
+}
+
